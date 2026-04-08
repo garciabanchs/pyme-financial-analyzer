ddc2c56bd09b8112e6d6e270917e0bec12453246
garciabanchs <garciabanchs@econometrica.com.ve>
2026-04-08 23:48:40 +0200

    Update ledger.py

--- ledger.py
+++ ledger.py
@@ -2,12 +2,22 @@ def construir_ledger(documentos):
     ledger = []
 
     for doc in documentos:
+        if doc["tipo"] == "factura_venta":
+            naturaleza = "entrada"
+        elif doc["tipo"] == "factura_compra":
+            naturaleza = "salida"
+        elif doc["tipo"] == "extracto_bancario":
+            naturaleza = "movimiento"
+        else:
+            naturaleza = "revisar"
+
         for monto in doc["importes"]:
             ledger.append({
                 "archivo": doc["archivo"],
                 "tipo": doc["tipo"],
                 "fecha": doc["fecha"],
-                "importe": monto
+                "importe": monto,
+                "naturaleza": naturaleza
             })
 
     return ledger
