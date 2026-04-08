13f767a22b30352a422bf88da68da70e86003b44
garciabanchs <garciabanchs@econometrica.com.ve>
2026-04-08 23:18:18 +0200

    Create extractor.py

---
+++ extractor.py
@@ -0,0 +1,14 @@
+from pypdf import PdfReader
+import os
+
+def extraer_texto_pdf(ruta_pdf):
+    texto = ""
+    try:
+        reader = PdfReader(ruta_pdf)
+        for page in reader.pages:
+            contenido = page.extract_text()
+            if contenido:
+                texto += contenido.lower()
+    except:
+        pass
+    return texto
