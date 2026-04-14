7e4c41832a647e6157e6953715a4ecf88ec5459a
garciabanchs <garciabanchs@econometrica.com.ve>
2026-04-14 20:54:14 +0200

    Update pdf_report.py

--- pdf_report.py
+++ pdf_report.py
@@ -1,6 +1,6 @@
+import os
 from weasyprint import HTML
 
-
 def generar_pdf_ejecutivo(pdf_path, html_string):
-    HTML(string=html_string).write_pdf(pdf_path)
-
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    HTML(string=html_string, base_url=base_dir).write_pdf(pdf_path)
