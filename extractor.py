ebc3f7c8d4bcadcdcf997dc68e674ffdc7dc1e90
garciabanchs <garciabanchs@econometrica.com.ve>
2026-04-18 14:24:31 +0200

    Update and rename extractor_editar.py to extractor.py

--- extractor.py
+++ extractor.py
@@ -8,7 +8,7 @@ def extraer_texto_pdf(ruta_pdf):
         for page in reader.pages:
             contenido = page.extract_text()
             if contenido:
-                texto += contenido + "\n"
-    except Exception:
+                texto += contenido.lower()
+    except:
         pass
     return texto
